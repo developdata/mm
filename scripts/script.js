78fd5852719af0d18eb33da5cf957df2d00cf6fd
developdata <isk9@hotmail.com>
2025-04-21 19:18:37 +0100

    email added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    var user = "contact";
-    var domain = "example.com";
+    var user = "musicalmediclab";
+    var domain = "gmail.com";
     var email = user + "@" + domain;
     var emailElement = document.getElementById("email-address");
     emailElement.innerHTML = '<a href="mailto:' + email + '">' + email + '</a>';
